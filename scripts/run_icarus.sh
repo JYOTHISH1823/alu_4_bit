faf9cb902d3387b37038dc29ded97a9beff65f36
JYOTHISH1823 <jyothish1823@gmail.com>
2025-08-19 16:18:20 +0530

    Updated ALU design and testbench

--- scripts/run_icarus.sh
+++ scripts/run_icarus.sh
@@ -7,3 +7,7 @@ vvp sim/alu_tb.out
 
 # View waveform
 gtkwave sim/alu.vcd &
+# this line opens the waveform viewer in the background
+#can be done in linux or ubuntu
+# if you are using windows, you may need to adjust the command accordingly
+# or use a different waveform viewer compatible with your system
